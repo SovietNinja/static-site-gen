2e371952222d3f17fc1fb68d899031d7cfdd0082
sovietninja <sovietninjaru@gmail.com>
2025-05-27 19:37:12 +0000

    cleaned textnode

--- src/textnode.py
+++ src/textnode.py
@@ -1,5 +1,5 @@
 from enum import Enum
-from htmlnode import LeafNode
+
 
 class TextType(Enum):
     TEXT = "text"
@@ -28,24 +28,7 @@ class TextNode:
             )
         return False
     
-    # def text_node_to_html_node(self,text_node):
-    #     if not isinstance(text_node,TextNode):
-    #         raise Exception("node is not TextNode instance")
-    #     match text_node.text_type:
-    #         case (text_node.text_type.TEXT):
-    #             return LeafNode(None,text_node.text)
-    #         case (text_node.text_type.BOLD):
-    #             return LeafNode("b",text_node.text)
-    #         case (text_node.text_type.ITALIC):
-    #             return LeafNode("i",text_node.text)
-    #         case (text_node.text_type.CODE):
-    #             return LeafNode("code",text_node.text)
-    #         case (text_node.text_type.LINK):
-    #             return LeafNode("a",text_node.text,{"href":f"{text_node.url}"})
-    #         case (text_node.text_type.IMAGE):
-    #             return LeafNode("img","",{"src":f"{text_node.url}",f"alt":f"{text_node.text}"}) 
-    #         case _:
-    #             raise Exception("Node type is not supported")
+
         
         
 
